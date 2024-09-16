75839a61be2956d8b598bb294905cac5718b42e1
akorosov <korosov@gmail.com>
2024-09-16 09:40:55 +0200

    update validation_summary to proc all sourcers

--- validate.sh
+++ validate.sh
@@ -6,6 +6,6 @@ export DMI_REFERENCE_DIR=/Data/sat/auxdata/ice_charts/cmems_obs-si_arc_physic_nr
 export OSISAF_REFERENCE_DIR=/Data/sim/data/OSISAF_ice_conc_amsr
 
 # ./validate.py AUTOMATIC_PRODUCT REFERENCE_PRODUCT START_DATE END_DATE
-./validate.py DMI NIC dmi_nic 2021-01-01 2021-03-31
-./validate.py DMI DMI dmi_dmi 2021-01-01 2021-03-31
-./validate.py DMI OSISAF dmi_osisaf 2021-01-01 2021-03-31
+./validate.py DMI NIC dmi_nic 2021-01-01 2021-12-31
+./validate.py DMI DMI dmi_dmi 2021-01-01 2021-12-31
+./validate.py DMI OSISAF dmi_osisaf 2021-01-01 2021-12-31
